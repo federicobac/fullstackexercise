c771d444de0c9b242c864880019719ca299c160a
federicobac <federico.baccaro1@gmail.com>
2026-09-11 12:17:19 +0200

    Add MyAmazing API endpoints
    Introduces a new MyAmazingController and MyAmazingService to expose entity data through an HTTP GET endpoint. The service reads from MyAmazingDatabase and returns the list of MyAmazingEntities to the API layer.

---
+++ server/API/MyAmazingController.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace API;
+
+public class MyAmazingController(MyAmazingService service)
+{
+    [HttpGet(nameof(GetEntities))]
+    public List<MyAmazingEntities> GetEntities()
+    {
+        return service.GetEntities();
+    }
+    
+}
---
+++ server/Service/MyAmazingService.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure;
+
+namespace Service;
+
+public class MyAmazingService(MyAmazingDatabase db)
+{
+    public List<MyAmazingEntities> GetEntities()
+    {
+        return db.MyAmazingEntities().ToList();
+    }
+}
